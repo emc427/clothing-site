d370a9f76b83992cef8af6dfd403f57aba447d66
Emily Chen <peizhangxumeilingchen@peis-MacBook-Air.local>
2019-03-31 16:56:14 -0700

    Updates

---
+++ precache-manifest.a598dcaf9b5cc3304b005d83d50009af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4205c89d78730eb3f4c7",
+    "url": "/clothing-site/static/css/main.116f7529.chunk.css"
+  },
+  {
+    "revision": "4205c89d78730eb3f4c7",
+    "url": "/clothing-site/static/js/main.4205c89d.chunk.js"
+  },
+  {
+    "revision": "c9969d5a64d9c4067cb0",
+    "url": "/clothing-site/static/css/1.b32ac1dc.chunk.css"
+  },
+  {
+    "revision": "c9969d5a64d9c4067cb0",
+    "url": "/clothing-site/static/js/1.c9969d5a.chunk.js"
+  },
+  {
+    "revision": "0c0b3a3337e855b0136b",
+    "url": "/clothing-site/static/js/runtime~main.0c0b3a33.js"
+  },
+  {
+    "revision": "3b7a7d50f9484899ef128218b9d497ef",
+    "url": "/clothing-site/static/media/cropped.3b7a7d50.png"
+  },
+  {
+    "revision": "27d6be56e1c11346a497277b9af312bf",
+    "url": "/clothing-site/static/media/cropped2.27d6be56.png"
+  },
+  {
+    "revision": "57930e3a60fbd317ab19de4bcb71dbe4",
+    "url": "/clothing-site/static/media/titlepic.57930e3a.png"
+  },
+  {
+    "revision": "fd9d77b5c4c1c823c3c52b09612350fc",
+    "url": "/clothing-site/index.html"
+  }
+];
